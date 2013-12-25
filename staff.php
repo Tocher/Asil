01634f842f169d31ba047c87467d64feb9b3e25b
stobe1 <markez1@mail.ru>
2013-12-25 22:55:43 +0300

    staff
    dsadsadas

---
+++ staff.php
@@ -0,0 +1,50 @@
+<?php include("header.php"); ?>
+		<article class="row">
+			<div class="container">
+				<h2 class="col-lg-10 col-xs-12 col-sm-10 col-lg-offset-2 col-xs-offset-0 col-sm-offset-2">ООО «<span>НПО АСИЛ</span>»</h2>
+				<div class="col-lg-2 col-xs-3 col-sm-3 col-md-2">
+					<ul>
+						<li><a href="#">отзывы клиентов</a></li>
+						<li><a href="#">лицензии</a></li>
+						<li><a href="#" class="active">наши сотрудники</a></li>
+						<li><a href="#">вакансии</a></li>
+
+					</ul>
+				</div>
+
+				<div class="col-lg-10 col-xs-8 col-xs-offset-1 col-sm-offset-0 col-sm-9 col-md-10">
+					<p>Pellentesque ac justo cursus massa mattis mollis ac in risus. Proin gravida erat ac elit tincidunt luctus. Praesent nibh metus, condimentum non dui quis, scelerisque viverra quam. Curabitur suscipit lobortis tellus, eget pulvinar mauris convallis sed. Sed eget leo sagittis, ultrices diam et, aliquet ante. Donec erat tortor, accumsan non eleifend at, blandit a odio. Integer sed nunc scelerisque, varius dolor nec, suscipit leo. Donec rutrum suscipit hendrerit. Integer pharetra egestas pulvinar. Proin porttitor ante enim, sit amet tincidunt enim rhoncus et. Curabitur sodales ultricies ligula, id rhoncus mi consequat quis. Sed faucibus fermentum turpis, nec cursus arcu tristique et. Sed tristique non quam ut tincidunt.</p>
+				</div>
+				<div class="col-lg-10 col-xs-12 col-sm-10 col-lg-offset-2 col-xs-offset-0 col-sm-offset-2" style="margin-top:50px;">
+					<div class="picture-wrap"><div class="image-wrap"><div class="image-staff image1"></div></div><span>Саторов Б.</span><br>исполнительный директор</div>
+					<div class="picture-wrap"><div class="image-wrap"><div class="image-staff image2"></div></div><span>Юлдошов А.</span><br>бухгалтер</div>
+					<div class="picture-wrap"><div class="image-wrap"><div class="image-staff image3"></div></div><span>Иброгимов И.</span><br>старший бригадир</div>
+					<div class="picture-wrap"><div class="image-wrap"><div class="image-staff image4"></div></div><span>Ахроров В. </span><br>старший инженер</div>
+					<div class="picture-wrap"><div class="image-wrap"><div class="image-staff image5"></div></div><span>Рахмонов А.</span><br>бригадир</div>
+					
+					<div class="picture-wrap"><div class="image-wrap"><div class="image-staff image6"></div></div><span>Рахмонов А.</span><br>бригадир</div>
+					<div class="picture-wrap"><div class="image-wrap"><div class="image-staff image7"></div></div><span>Хасанов А.</span><br>бригадир</div>
+					<div class="picture-wrap"><div class="image-wrap"><div class="image-staff image8"></div></div><span>Ишанов Д.</span><br>инженер</div>
+					<div class="picture-wrap"><div class="image-wrap"><div class="image-staff image9"></div></div><span>Махмадшоев Ф. </span><br>инженер</div>
+					<div class="picture-wrap"><div class="image-wrap"><div class="image-staff image10"></div></div><span>Атаджанов Ф. </span><br>инженер</div>
+				
+					<div class="picture-wrap"><div class="image-wrap"><div class="image-staff image11"></div></div><span>Атаджанов Ф. </span><br>специалист широкого профиля</div>
+					<div class="picture-wrap"><div class="image-wrap"><div class="image-staff image12"></div></div><span>Насырова А.  </span><br>специалист широкого профиля</div>
+					<div class="picture-wrap"><div class="image-wrap"><div class="image-staff image13"></div></div><span>Хакимов Х. </span><br>завсклад</div>
+					<div class="picture-wrap"><div class="image-wrap"><div class="image-staff image14"></div></div><span>Ахмедов А.   </span><br>офис менеджер</div>
+					<div class="picture-wrap"><div class="image-wrap"><div class="image-staff image15"></div></div><span>Шарапов А. </span><br>фрезировщик</div>
+					
+					<div class="picture-wrap"><div class="image-wrap"><div class="image-staff image16"></div></div><span>Шарапов А  </span><br>фрезировщик</div>
+					<div class="picture-wrap"><div class="image-wrap"><div class="image-staff image17"></div></div><span>Шарипов М. </span><br>сантехник</div>
+					<div class="picture-wrap"><div class="image-wrap"><div class="image-staff image18"></div></div><span>Санчаров М.   </span><br>завсклад</div>
+
+				</div>
+			</div>
+		</article>
+		
+
+
+
+
+<?php include("footer.php"); ?>
+	
