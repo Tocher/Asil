8db5d025d313b8cd4d624c0c204a8ea315d1b1af
Tocher <tocher99@gmail.com>
2014-07-25 21:01:21 +0300

    phpshka :panda_face:

---
+++ footer.php
@@ -0,0 +1,89 @@
+		<div class="row clients">
+			<div class="container">
+				<h3>среди наших клиентов</h3>
+				<img src="img/brand1.jpg" alt="#">
+				<img src="img/brand2.jpg" alt="#">
+				<img src="img/brand3.jpg" alt="#">
+				<img src="img/brand4.jpg" alt="#">
+				<img src="img/brand5.jpg" alt="#">
+			</div>
+		</div>
+		<footer class="row">
+			<div class="container">
+				<div class="col-lg-3 col-md-3 col-sm-3 col-xs-6 active">
+					<ul>
+						<h5><a href="#" class="active">главная</a></h5>
+					</ul>
+				</div>
+				<div class="col-lg-3 col-md-3 col-sm-3 col-xs-6">
+					<ul>
+						<h5><a href="#">о компании</a></h5>
+						<li><a href="#">слово директора</a></li>
+						<li><a href="#">отзывы клиентов</a></li>
+						<li><a href="#">лицензии</a></li>
+						<li><a href="#">команда</a></li>
+						<li><a href="#">вакансии</a></li>
+					</ul>
+				</div>
+				<div class="col-lg-3 col-md-3 col-sm-3 col-xs-6">
+					<ul>
+						<h5><a href="#" class="">направления</a></h5>
+						<li><a href="#">apolitex</a></li>
+						<li><a href="#">apolimerStone</a></li>
+						<li><a href="#">astone</a></li>
+						<li><a href="#">aporolon</a></li>
+						<li><a href="#">aoil</a></li>
+						<li><a href="#">abuild</a></li>
+					</ul>
+				</div>
+				<div class="col-lg-3 col-md-3 col-sm-3 col-xs-6">
+					<h5><a href="">контактная информация</a></h5>
+					<p class="addres">Таджикистан,<br> г. Ходжент,<br> ул. XXXXXXXXXXXXXXXX</p>
+					<a href="#" class="look-map"><span>посмотреть на карте</span></a>
+					<div class="phone">(+992 44) 600-XX-XX</div>
+					<a href="mailto:#" class="email">info@asil.tj</a>
+				</div>
+			</div>
+		</footer>
+		<div class="row copy">
+			<div class="container">
+				<div class="row">
+					<p class="col-lg-6 col-xs-12 col-sm-8 col-md-6">© 2005...<span class="year"></span>, ООО «Научно производственное объединение Асил»</p>
+					<p class="col-lg-2 col-lg-offset-2 col-sm-offset-0 col-md-offset-2 col-sm-3 col-xs-4 col-xs-offset-4"><button id="trigger-overlay-social" type="button">поделиться ссылкой</button></p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="overlay overlay-hugeinc social">
+		<div>
+			<button type="button" class="overlay-close">Close</button>
+			<ul>
+				<h5>Поделиться ссылкой</h5>
+				<li><a href="#" class="facebook"><span></span>Facebook</a></li>
+				<li><a href="#" class="twitter"><span></span>Twitter</a></li>
+				<li><a href="#" class="google-plus"><span></span>Google Plus</a></li><br>
+				<li><a href="#" class="live-journal"><span></span>LiveJournal</a></li>
+				<li><a href="#" class="vkontakte"><span></span>Вконтакте</a></li>
+				<li><a href="#" class="odnoklassniki"><span></span>Одноклассники</a></li>
+			</ul>
+		</div>
+	</div>
+	<div class="overlay overlay-hugeinc letter">
+		<div>
+			<button type="button" class="overlay-close">Close</button>
+			<h5>Написать письмо</h5>
+			<form action="action.php" method="GET">
+				<label><input type="text" name="name"><span>ваше имя</span></label>
+				<label><input type="text" name="phone"><span>номер телефона</span></label>
+				<label><input type="text" name="email"><span>эл. почта</span></label>
+				<label><input type="text" name="message"><span>сообщение</span></label>
+				<input type="submit" value="отправить">
+			</form>
+		</div>
+	</div>
+
+	<script src="js/classie.js"></script>
+	<script src="js/main.js"></script>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/style2.css">
+	<link href="img/favicon.ico" rel="shortcut icon" type="image/x-icon" />
+	<script src="js/jquery-1.11.1.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/modernizr.custom.js"></script>
+	<!--<script src="js/jquery.smslider.min.js"></script>-->
+	<script src="js/scripts.js"></script>
+	<title>Научно производственное объединение Асил - Главная</title>
+</head>
+<body>
+	<div class="container-fluid">
+		<header class="row">
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-2 col-lg-2 logo"><p>научно<br>производственное<br>объединение</p></div>
+					<div class="col-xs-1 col-xs-offset-1 col-sm-offset-2 col-md-offset-3 lang"><span class="active">RUS</span></div>
+					<div class="col-xs-1 col-xs-offset-1 col-sm-offset-0 lang"><a href="#">ENG</a></div>
+					<div class="col-lg-3 col-md-4 col-sm-5 col-xs-12 col-lg-offset-2 phone pull-right"><p><span>(+992 44)</span> 600-XX-XX</p><a href="#" id="trigger-overlay-letter">написать письмо</a></div>
+				</div>
+			</div>
+		</header>
+		<nav class="row">
+			<div class="container">
+				<div class="row">
+					<ul class="list-inline">
+						<li class="col-lg-2 col-xs-6 col-sm-3"><a href="" class="active">главная</a></li>
+						<li class="col-lg-2 col-xs-6 col-sm-3 col-lg-offset-1"><a href="#">о компании </a></li>
+						<li class="col-lg-3 col-xs-6 col-sm-3 col-lg-offset-1"><a href="#">направления</a></li>
+						<li class="col-lg-2 col-xs-6 col-sm-3 col-lg-offset-1"><a href="#">контактная информация</a></li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+		<div class="custom-select" style="display:none;">
+			<select id="fxselect" name="fxselect">
+				<option value="-1">Choose an effect...</option>
+				<option value="fxSoftScale" selected>Soft scale</option>
+				<option value="fxPressAway">Press away</option>
+				<option value="fxSideSwing">Side Swing</option>
+				<option value="fxFortuneWheel">Fortune wheel</option>
+				<option value="fxSwipe">Swipe</option>
+				<option value="fxPushReveal">Push reveal</option>
+				<option value="fxSnapIn">Snap in</option>
+				<option value="fxLetMeIn">Let me in</option>
+				<option value="fxStickIt">Stick it</option>
+				<option value="fxArchiveMe">Archive me</option>
+				<option value="fxVGrowth">Vertical growth</option>
+				<option value="fxSlideBehind">Slide Behind</option>
+				<option value="fxSoftPulse">Soft Pulse</option>
+				<option value="fxEarthquake">Earthquake</option>
+				<option value="fxCliffDiving">Cliff diving</option>
+			</select>
+		</div>		
