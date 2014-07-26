835a721b87257e51c99999150fa435fe737919b9
stobe1 <markez1@mail.ru>
2014-07-26 23:16:09 +0300

    sffgjhhjl
    ghjfdsa

--- staff.php
+++ staff.php
@@ -1,9 +1,45 @@
 <?php include("header.php"); ?>
+		<nav class="row">
+			<div class="container">
+				<div class="row">
+					<ul class="list-inline">
+						<li class="col-lg-2 col-xs-6 col-sm-3"><a href="/" class="active">главная</a></li>
+						<li class="col-lg-2 col-xs-6 col-sm-3 col-lg-offset-1"><a href="about.php">о компании </a></li>
+						<li class="col-lg-3 col-xs-6 col-sm-3 col-lg-offset-1"><a href="directions.php">направления</a></li>
+						<li class="col-lg-2 col-xs-6 col-sm-3 col-lg-offset-1"><a href="contact_information.php">контактная информация</a></li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+		<div class="custom-select" style="display:none;">
+			<select id="fxselect" name="fxselect">
+				<option value="-1">Choose an effect...</option>
+				<option value="fxSoftScale" selected>Soft scale</option>
+				<option value="fxPressAway">Press away</option>
+				<option value="fxSideSwing">Side Swing</option>
+				<option value="fxFortuneWheel">Fortune wheel</option>
+				<option value="fxSwipe">Swipe</option>
+				<option value="fxPushReveal">Push reveal</option>
+				<option value="fxSnapIn">Snap in</option>
+				<option value="fxLetMeIn">Let me in</option>
+				<option value="fxStickIt">Stick it</option>
+				<option value="fxArchiveMe">Archive me</option>
+				<option value="fxVGrowth">Vertical growth</option>
+				<option value="fxSlideBehind">Slide Behind</option>
+				<option value="fxSoftPulse">Soft Pulse</option>
+				<option value="fxEarthquake">Earthquake</option>
+				<option value="fxCliffDiving">Cliff diving</option>
+			</select>
+		</div>	
+		<div class="row component component-fullwidth-child" id="component">
+			<img src="img/slide-child.jpg">
+		</div>
 		<article class="row">
 			<div class="container">
-				<h2 class="col-lg-10 col-xs-12 col-sm-10 col-lg-offset-2 col-xs-offset-0 col-sm-offset-2">ООО «<span>НПО АСИЛ</span>»</h2>
+				<div class="breadcrumbs col-lg-10 col-xs-12 col-sm-10 col-lg-offset-2 col-xs-offset-0 col-sm-offset-2"><a href="">главная</a> / <a href="">о компании</a> /</div>
+				<h2 class="col-lg-10 col-xs-12 col-sm-10 col-lg-offset-2 col-xs-offset-0 col-sm-offset-2"><span>Наши сотрудники</span></h2>
 				<div class="col-lg-2 col-xs-3 col-sm-3 col-md-2">
-					<ul>
+					<ul class="child">
 						<li><a href="#">отзывы клиентов</a></li>
 						<li><a href="#">лицензии</a></li>
 						<li><a href="#" class="active">наши сотрудники</a></li>
@@ -41,7 +77,43 @@
 				</div>
 			</div>
 		</article>
-		
+		<footer class="row">
+			<div class="container">
+				<div class="col-lg-3 col-md-3 col-sm-3 col-xs-6">
+					<ul>
+						<h5><a href="#">главная</a></h5>
+					</ul>
+				</div>
+				<div class="col-lg-3 col-md-3 col-sm-3 col-xs-6 active">
+					<ul>
+						<h5><a href="#" class="active">о компании</a></h5>
+						<li><a href="#">слово директора</a></li>
+						<li><a href="#">отзывы клиентов</a></li>
+						<li><a href="#">лицензии</a></li>
+						<li><a href="#">команда</a></li>
+						<li><a href="#">вакансии</a></li>
+					</ul>
+				</div>
+				<div class="col-lg-3 col-md-3 col-sm-3 col-xs-6">
+					<ul>
+						<h5><a href="#" class="">направления</a></h5>
+						<li><a href="#">apolitex</a></li>
+						<li><a href="#">apolimerStone</a></li>
+						<li><a href="#">astone</a></li>
+						<li><a href="#">aporolon</a></li>
+						<li><a href="#">aoil</a></li>
+						<li><a href="#">abuild</a></li>
+					</ul>
+				</div>
+				<div class="col-lg-3 col-md-3 col-sm-3 col-xs-6">
+					<h5><a href="">контактная информация</a></h5>
+					<p class="addres">Таджикистан,<br> г. Ходжент,<br> ул. XXXXXXXXXXXXXXXX</p>
+					<a href="#" class="look-map"><span>посмотреть на карте</span></a>
+					<div class="phone">(+992 44) 600-XX-XX</div>
+					<a href="mailto:#" class="email">info@asil.tj</a>
+				</div>
+			</div>
+		</footer>
 
 
 
